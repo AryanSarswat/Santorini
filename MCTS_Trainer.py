19e80126864337b5f7ac06cb114d68e209a71559
Master-Procrastinator <65006917+Master-Procrastinator@users.noreply.github.com>
2021-03-08 11:04:33 +0800

    added execute function

--- MCTS_Trainer.py
+++ MCTS_Trainer.py
@@ -15,16 +15,37 @@ class Trainer:
         self.mcts = MCTS(self.game, self.model, self.args)
 
     def execute_episode(self):
+        train_examples = []
+        current_player = self.game.get_current_player()
+        state = self.game
+
+        while True:
+            self.mcts = MCTS(self.game,self.model,self.args)
+            root = self.mcts.run(self.model,state,current_player)
+
+            state = root.select_child()
+            current_player = state.get_current_player()
+
         pass
 
     def learn(self):
-        pass
+        for i in range(self.args['numIters']):
+            train_examples = []
+            for eps in range(self.args['numEps']):
+                iteration_train_examples = self.execute_episode()
+                train_examples.extend(iteration_train_examples)
+            
+            shuffle(train_examples)
+            self.train(train_examples)
+            filename = self.args["checkpoint_path"]
+            self.save_checkpoint(folder=os.getcwd(),filename)
 
     def train(self):
         pass
 
     def loss_v(self,targets,outputs):
-        pass
+        loss = torch.sum((targets-outputs.view(-1))**2)/target.size([0])
+        return loss
 
     def save_checkpoint(self,folder,filename):
         """
