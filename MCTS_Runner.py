b87ee6c5d45778760c3f2ad2b9b0ad1c2ce2278b
Master-Procrastinator <65006917+Master-Procrastinator@users.noreply.github.com>
2021-03-13 16:50:59 +0800

    Added Runner code to train AI

--- MCTS_Runner.py
+++ MCTS_Runner.py
@@ -1,7 +1,7 @@
 from Game import *
 from MCTS import MCTS
-
-
+from MCTS_Trainer import Agent
+from MCTS_NN import Neural_Network
 A = HumanPlayer("A")
 B = HumanPlayer("B")
 
@@ -13,15 +13,12 @@ board.PlayerB.place_workers(board)
 board.print_board()
 
 args = {
-    'batch_size': 64,
-    'numIters': 5,                                # Total number of training iterations
+    'numIters': 10,                                # Total number of training iterations
     'Num_Simulations': 100,                     # Total number of MCTS simulations to run when deciding on a move to play
-    'numEps': 5,                                  # Number of full games (episodes) to run during each iteration
-    'numItersForTrainExamplesHistory': 20,
     'epochs': 2,                                    # Number of epochs of training per iteration
     'checkpoint_path': 'latest.pth'
 }
 
-mcts = MCTS(board,"Model",args)
+AI = Agent(board,Neural_Network(),args)
 
-root = mcts.run("Model",board,board.Player_turn())
+AI.train()
