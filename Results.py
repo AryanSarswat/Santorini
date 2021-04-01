c862eddfc4904cb40c491b1df07fac03d39b31b8
Master-Procrastinator <65006917+Master-Procrastinator@users.noreply.github.com>
2021-04-01 16:48:34 +0800

    Updated CNN_Trainer to add actions

---
+++ Results.py
@@ -0,0 +1,10 @@
+from Combined import run_santorini,LinearRlAgentV2,MCTS_Only_Agent,Trainer_CNN
+from MCTS_Trainer import MCTS_Agent
+
+MCTS_ANN_A = MCTS_Agent("A")
+MCTS_ANN_B = MCTS_Agent("B")
+
+Linear_A = LinearRlAgentV2("A")
+Linear_B = LinearRlAgentV2("A")
+
+Trainer_CNN_A = Trainer_CNN()
